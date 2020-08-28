6ad8cd2bb227a2b5464218c3c2f04c77d4e40136
bernard-ng <ngandubernard@gmail.com>
2020-08-28 17:22:26 +0200

    feat(app): loader interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeadersInterceptor} from './interceptors/headers.interceptor';
+import {LoaderComponent} from './components/loader/loader.component';
+import {LoaderService} from './services/loader.service';
+import {LoaderInterceptor} from './interceptors/loader.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +22,9 @@ import {HeadersInterceptor} from './interceptors/headers.interceptor';
   providers: [
     [
       {provide: HTTP_INTERCEPTORS, useClass: HeadersInterceptor, multi: true},
-    ]
+      {provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true}
+    ],
+    LoaderService
   ],
   bootstrap: [AppComponent]
 })
